d37ea157c3701f839b397f8590c933e9e01b6c44
ShengyunYu <yushengyun2013@163.com>
2019-01-16 15:06:00 +0800

    Date:20190116-1 INFO: new 2 7 9 12 14 20 21 26 27 35 58

---
+++ 0.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 虞圣赟 on 2019/1/16.
+//
+
+#include "common.h"
+
+
+int main(){
+    Solution s;
+
+}
---
+++ common.h
@@ -0,0 +1,25 @@
+//
+// Created by 虞圣赟 on 2019/1/14.
+//
+
+#ifndef LEETCODE_COMMON_H
+#define LEETCODE_COMMON_H
+
+#endif //LEETCODE_COMMON_H
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <set>
+
+
+using namespace std;
+
+
