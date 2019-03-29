0341075482504440dce3defe5881f51918850dac
ShengyunYu <yushengyun2013@163.com>
2019-03-29 20:20:05 +0800

    Date:20190329-2
    INFO: new 0206 0226 0231 0234

---
+++ 0028.cpp
@@ -0,0 +1,19 @@
+//
+// Created by ShengyunYu on 2019/3/29.
+//
+
+#include "common.h"
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+    }
+};
+
+
+
+
+int main(){
+
+}
