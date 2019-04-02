6f0b25ca09c13515a21eb99be2f4d9c2741dc0e2
yushengyun@163.com <yushengyun2013@163.com>
2019-04-02 21:45:41 +0800

    Date:20190402-1
    INFO: new 0292 0303 0387 0520
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

--- 0303.cpp
+++ 0303.cpp
@@ -7,8 +7,10 @@
 
 class NumArray {
 public:
-    NumArray(vector<int>& nums) {
-
+    NumArray(const vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            data.push_back(nums[i]);
+        }
     }
 
     int sumRange(int i, int j) {
@@ -20,9 +22,17 @@ public:
     }
 
 private:
-    vector<int> sum;
+    vector<int> data;
 };
 
+
+int main(){
+    vector<int> nums{-2, 0, 3, -5, 2, -1};
+    NumArray* obj = new NumArray(nums);
+    cout << obj->sumRange(0, 2) << endl;
+    cout << obj->sumRange(2, 5) << endl;
+    cout << obj->sumRange(0, 5) << endl;
+}
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray* obj = new NumArray(nums);
