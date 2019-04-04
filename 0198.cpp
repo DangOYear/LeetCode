c11877d9eb40c422fc7be720882fbaf69431a736
yushengyun@163.com <yushengyun2013@163.com>
2019-04-04 11:04:28 +0800

    Date:20190404-1
    INFO: new 0198
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

--- 0198.cpp
+++ 0198.cpp
@@ -7,12 +7,23 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-
+        int len = nums.size();
+        if (0 == len)
+            return 0;
+        if (1 == len)
+            return nums[0];
+        int dp[len + 2];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < len; ++i) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[len - 1];
     }
 };
 
 int main(){
     Solution s;
-    vector<int> nums{2,7,9,3,1};
+    vector<int> nums{2,1,1,2};
     cout << s.rob(nums) << endl;
 }
