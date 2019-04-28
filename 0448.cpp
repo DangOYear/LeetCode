578a02b66a87553819a54804bcc26bab4d8239d3
yushengyun@163.com <yushengyun2013@163.com>
2019-04-28 13:59:27 +0800

    Date:20190428-1
    INFO: new 0448 0485 0606 0693 0697 0703 0832 0868 0942 0944 0953 1005
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

--- 0448.cpp
+++ 0448.cpp
@@ -8,15 +8,36 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int> res;
-        int size = nums.size();
-        int flag[size] = {0};
-        for (int i = 0; i < size; ++i) {
-            flag[nums[i]]++;
+        for (int i = 0; i < nums.size(); ++i) {
+            while (i + 1 != nums[i] && nums[nums[i] - 1] != nums[i])
+                swap(&nums[nums[i] - 1], &nums[i]);
         }
-        for (int j = 0; j < ; ++j) {
-            
+
+        for (int j = 0; j < nums.size(); ++j) {
+            if (j + 1 != nums[j])
+                res.push_back(j + 1);
         }
+        return res;
+    }
+
+    void swap(int* a, int* b){
+        int temp = *a;
+        *a = *b;
+        *b = temp;
     }
 };
+
+
+void Print(vector<int> res){
+    for (int i = 0; i < res.size(); ++i) {
+        cout << res[i] << " ";
+    }
+}
+
+int main(){
+    vector<int> nums{4,3,2,7,8,2,3,1};
+    Solution s;
+    Print(s.findDisappearedNumbers(nums));
+}
 
 
---
+++ 0697.cpp
@@ -0,0 +1,19 @@
+//
+// Created by ShengyunYu on 2019/4/28.
+//
+
+#include "common.h"
+
+
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+
+    }
+};
+
+int main(){
+    vector<int> nums{1,2,2,3,1,4,2};
+    Solution s;
+    cout << s.findShortestSubArray(nums) << endl;
+}
