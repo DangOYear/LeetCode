92c2c18d5ab8c78fc58664ddcfef8bb4be06cc20
yushengyun@163.com <yushengyun2013@163.com>
2019-05-01 15:29:31 +0800

    Date:20190501-1
    INFO: new 0530 0628 0896
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

--- 0697.cpp
+++ 0697.cpp
@@ -8,7 +8,28 @@
 class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
+        if (nums.size() == 1)
+            return 1;
+        map<int, int> left;
+        map<int, int> right;
+        map<int, int> count;
+        int Max = -1;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (left[nums[i]] == 0) {
+                left[nums[i]] = i + 1;
+            } else{
+                right[nums[i]] = i + 1;
+            }
+            ++count[nums[i]];
+            Max = max(count[nums[i]], Max);
+        }
 
+        int Min = nums.size();
+        for (auto it = count.begin(); it != count.end(); ++it) {
+            if (it->second == Max)
+                Min = min(Min, right[it->first] - left[it->first] + 1);
+        }
+        return Min;
     }
 };
 
