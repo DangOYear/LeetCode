a6b85f1d1772c9d5a6da39c847918686d4ea73cc
yushengyun@163.com <yushengyun2013@163.com>
2019-05-04 16:14:51 +0800

    Date:20190504-2
    INFO: new 0551 0806 1013
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ 0496.cpp
@@ -0,0 +1,25 @@
+//
+// Created by ShengyunYu on 2019/5/4.
+//
+
+
+
+#include "common.h"
+
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        map
+        for (int i = 0; i < ; ++i) {
+
+        }
+    }
+};
+
+
+int main(){
+    vector<int> nums1{4,1,2};
+    vector<int> nums2{1,3,4,2};
+
+}
---
+++ 0830.cpp
@@ -0,0 +1,37 @@
+//
+// Created by ShengyunYu on 2019/5/4.
+//
+#include "common.h"
+
+
+class Solution {
+public:
+    vector<vector<int>> largeGroupPositions(string S) {
+        vector<vector<int>> res;
+        for (int i = 0; i < S.length() - 1; ++i) {
+            if ()
+        }
+
+        return res;
+    }
+};
+
+void Print(vector<vector<int>> res){
+    cout << "[";
+    for (int i = 0; i < res.size(); ++i) {
+        cout << "[";
+        cout << res[i][0] << ",";
+        cout << res[i][1] << "]";
+    }
+    cout << "]" << endl;
+}
+
+int main(){
+    string S = "abbxxxxzzy";
+    Solution s;
+    Print(s.largeGroupPositions(S));
+    S = "abc";
+    Print(s.largeGroupPositions(S));
+    S = "abcdddeeeeaabbbcd";
+    Print(s.largeGroupPositions(S));
+}
