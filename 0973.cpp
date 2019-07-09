a39a6023eb5ec28d959c09cbc7fe91aa07607cca
yushengyun@163.com <yushengyun2013@163.com>
2019-07-09 13:04:33 +0800

    Date:20190709-1
    INFO:new 0215 0287 0973
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ 0973.cpp
@@ -0,0 +1,15 @@
+
+//
+// Created by ShengyunYu on 2019/7/9.
+//
+
+#include "common.h"
+
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+
+    }
+};
+
--- common.h
+++ common.h
@@ -93,3 +93,4 @@ void PrintLinkList(ListNode* root) {
 
 //0290可以优化
 //0933可以用二分优化
+//0215可以用快速排序进行优化
