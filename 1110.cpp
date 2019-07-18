905785e466922040f76fb4b5a646ca92090b0b4c
yushengyun@163.com <yushengyun2013@163.com>
2019-07-18 12:08:41 +0800

    Date:20190717-1
    INFO:0061 0445 0661 0950 1122
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ 1110.cpp
@@ -0,0 +1,22 @@
+//
+// Created by ShengyunYu on 2019/7/18.
+//
+
+#include "common.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+
+    }
+};
+
