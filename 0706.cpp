c2046c6b2d252debe4f3ffa6d4ef65993e12c12b
ShengyunYu <yushengyun2013@163.com>
2019-08-13 21:59:19 +0800

    Date:20190813-2
    INFO:0463 0706 1154
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

--- 0706.cpp
+++ 0706.cpp
@@ -8,23 +8,26 @@ class MyHashMap {
 public:
     /** Initialize your data structure here. */
     MyHashMap() {
-
+        memset(num, -1, sizeof(num));
     }
 
     /** value will always be non-negative. */
     void put(int key, int value) {
-
+        num[key] = value;
     }
 
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-
+        return num[key];
     }
 
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-
+        num[key] = -1;
     }
+
+private:
+    int num[1000010];
 };
 
 /**
