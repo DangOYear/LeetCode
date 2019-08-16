b24c3f4748f63ecc0baa4faaa771fd07dac87dc5
yushengyun@163.com <yushengyun2013@163.com>
2019-08-16 21:02:28 +0800

    Date:20190816-1
    INFO:0894 1110
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

--- 0894.cpp
+++ 0894.cpp
@@ -16,6 +16,28 @@
 class Solution {
 public:
     vector<TreeNode*> allPossibleFBT(int N) {
-
+        if (cache.count(N) == 0) {
+            vector<TreeNode*> temp;
+            if (N == 1) {
+                temp.push_back(new TreeNode(0));
+            } else if (N % 2 == 1) {
+                for (int i = 0; i < N; ++i) {
+                    int j = N - 1 - i;
+                    for (TreeNode *left : allPossibleFBT(i)) {
+                        for (TreeNode *right : allPossibleFBT(j)) {
+                            TreeNode* node = new TreeNode(0);
+                            node->left = left;
+                            node->right = right;
+                            temp.push_back(node);
+                        }
+                    }
+                }
+            }
+            cache[N] = temp;
+        }
+        return cache[N];
     }
+
+private:
+    map<int, vector<TreeNode*>> cache;
 };
--- 1110.cpp
+++ 1110.cpp
@@ -16,8 +16,33 @@
 class Solution {
 public:
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        if (root == NULL) return res;
+        for (int x : to_delete) s.insert(x);
 
+        TreeNode* first = solve(root);
+
+        if (first != NULL)  res.push_back(root);
+        return res;
     }
+
+    TreeNode* solve(TreeNode* root) {
+        if (root == NULL) return NULL;
+
+        root->left = solve(root->left);
+        root->right = solve(root->right);
+
+        if (s.count(root->val) != 0) {
+            if (root->left) res.push_back(root->left);
+            if (root->right)    res.push_back(root->right);
+            return NULL;
+        }
+        return root;
+    }
+
+private:
+    set<int> s;
+    vector<TreeNode*> res;
+
 };
 
 
