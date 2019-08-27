88c94e3ed6f72c10f8addb39e8e94948d0a6493b
yushengyun@163.com <yushengyun2013@163.com>
2019-08-27 15:01:32 +0800

    Date:20190827-1
    INFO:0861
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

--- 0004.cpp
+++ 0004.cpp
@@ -8,3 +8,12 @@ public:
 
     }
 };
+
+
+
+int main() {
+    vector<int> nums1{1, 2};
+    vector<int> nums2{3, 4};
+    Solution solution;
+    cout << solution.findMedianSortedArrays(nums1, nums2);
+}
--- 0861.cpp
+++ 0861.cpp
@@ -7,7 +7,18 @@
 class Solution {
 public:
     int matrixScore(vector<vector<int>>& A) {
-
+        int row = A.size();
+        int col = row == 0?0:A[0].size();
+        int res = (1 << (col - 1)) * row;
+        for (int j = 1; j < col; ++j) {
+            int count = 0;
+            for (int i = 0; i < row; ++i) {
+                if (A[i][j] == A[i][0])
+                    ++count;
+            }
+            res += max(count, row - count) * (1 << (col - 1 -j));
+        }
+        return res;
     }
 };
 
