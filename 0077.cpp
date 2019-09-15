fbf44dedd97d836e0f34d46bc5b8de46a04a6526
yushengyun@163.com <yushengyun2013@163.com>
2019-09-15 20:31:52 +0800

    Date:20190915-3
    INFO:0049 0077
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

--- 0077.cpp
+++ 0077.cpp
@@ -9,21 +9,46 @@
 class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> res;
-
-        if (k > n) {
+        res.clear();
+        if (k > n)
             return res;
-        }
-
-
+        backstrace({}, 1, n, k);
+        return res;
     }
 
 
-    void backtrace()
+    void backstrace(vector<int> temp, int count, int n, int k) {
+        if (temp.size() == k) {
+            res.push_back(temp);
+            return;
+        }
+
+        for (int i = count; i <= n; ++i) {
+            temp.push_back(i);
+            backstrace(temp, i + 1, n, k);
+            temp.pop_back();
+        }
+
+    }
+
+private:
+    vector<vector<int>> res;
 };
 
 
+//
+//Input: n = 4, k = 2
+//Output:
+//[
+//[2,4],
+//[3,4],
+//[2,3],
+//[1,2],
+//[1,3],
+//[1,4],
+//]
 
 int main(int argc, char **argv) {
-
+    Solution solution;
+    PrintIntVectorVector(solution.combine(4, 2));
 }
