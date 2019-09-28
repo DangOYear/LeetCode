1ae79487432c2d927e92c5502191091f3221f130
yushengyun@163.com <yushengyun2013@163.com>
2019-09-28 12:55:42 +0800

    Date:20190928-1
    INFO:0911 1130
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

--- 1130.cpp
+++ 1130.cpp
@@ -9,21 +9,24 @@
 class Solution {
 public:
     int mctFromLeafValues(vector<int>& arr) {
-        int leafValue = 0;
+        if (arr.size() < 2)
+            return 0;
         int res = 0;
-        priority_queue<int, vector<int>, greater<int> > pq;
-        for(int x:arr) {
-            pq.push(x);
-        }
+        stack<int> s;
+        s.push(INT_MAX);
 
-        while (pq.size() >= 0) {
-            temp *= pq.top();
-            pq.pop();
-            temp *= pq.top();
-            pq.pop();
-            pq.push(temp);
-            res += temp;
-            cout << temp << endl;
+        for (int num:arr) {
+            while (s.top() <= num) {
+                int mid = s.top();
+                s.pop();
+                res += mid * min(s.top(), num);
+            }
+            s.push(num);
+        }
+        while (s.size() > 2) {
+            int mid = s.top();
+            s.pop();
+            res += mid * s.top();
         }
         return res;
     }
