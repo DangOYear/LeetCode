632f29c9ac3e763bb2889159b5dd5f6389f1d498
yushengyun@163.com <yushengyun2013@163.com>
2019-11-04 08:15:05 +0800

    Date:20191104-1
    INFO:1243
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ 1237.cpp
@@ -0,0 +1,35 @@
+//
+// Created by ShengyunYu on 2019/10/27.
+//
+
+#include "common.h"
+
+interface CustomFunction {
+public:
+// Returns positive integer f(x, y) for any given positive integer x and y.
+int f(int x, int y);
+};
+
+
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ * public:
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     int f(int x, int y);
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+        vector<vector<int>> res;
+
+
+    }
+};
+
+
